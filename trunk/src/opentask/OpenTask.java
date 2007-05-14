a08fac3382fe71a79377491f86a44e97c63d1ebc
j_a_r <j_a_r@720f0306-7130-0410-9649-dcfab5623979>
2007-05-14 17:44:29 +0000

    initial

---
+++ trunk/src/opentask/OpenTask.java
@@ -0,0 +1,136 @@
+/* 
+ * OpenTask.java
+ * OpenTask
+ *
+ * Copyright (C) 2007 Jochen A. Rassler
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package opentask;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * @author rassler
+ *
+ */
+public class OpenTask implements ActionListener{
+
+	// important GUI elements
+	JPanel mainPanel;
+	JMenuBar menuBar;
+	
+	// important Data 
+	private boolean dirty;
+
+	public OpenTask() {
+		if (dirty)
+			;// save data
+	}
+	
+	public void saveData() {
+		
+	}
+	
+	private void createMenu() {
+		JMenu menu;
+		JMenuItem menuItem;
+		
+		menuBar = new JMenuBar();
+		// File Menu
+		menu = new JMenu("File");
+		menuBar.add(menu);
+		menuItem = new JMenuItem("Exit", KeyEvent.VK_I);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.ALT_MASK));
+        //menuItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+        menuItem.addActionListener(new CloseAction(this));
+        menu.add(menuItem);
+
+        // Task Menu
+        menu = new JMenu("Task");
+		menuItem = new JMenuItem("New", KeyEvent.VK_N);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK));
+        menu.add(menuItem);
+		menuItem = new JMenuItem("Edit", KeyEvent.VK_E);
+        menu.add(menuItem);
+		menuItem = new JMenuItem("Delete", KeyEvent.VK_D);
+        menu.add(menuItem);
+        menuBar.add(menu);
+
+        // Options Menu
+        menu = new JMenu("Options");
+        menuBar.add(menu);
+
+        // About Menu
+        menu = new JMenu("About");
+        menuBar.add(menu);
+	}
+	
+	private void createMainPane() {
+		mainPanel = new JPanel();
+		mainPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		mainPanel.add(new JLabel("Hallo!"));
+	}
+	
+	private static void createAndShowGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame window = new JFrame("OpenTask");
+
+		OpenTask task = new OpenTask();
+		task.createMenu();
+		task.createMainPane();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setJMenuBar(task.menuBar);
+		window.setContentPane(task.mainPanel);
+		
+		window.pack();
+		window.setVisible(true);
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
+    }
+	public void windowClosing(WindowEvent e) {		
+		saveData();
+	}
+	
+}
+
+class CloseAction implements ActionListener {
+	Object app;
+	public CloseAction(Object app) {
+		this.app = app;
+	}
+    public void actionPerformed(ActionEvent e) {
+    	if (app instanceof OpenTask) ((OpenTask)app).saveData();
+        System.exit(0);
+    }
+}
---
+++ trunk/src/opentask/data/ActionItem.java
@@ -0,0 +1,97 @@
+/* 
+ * ActionItem.java
+ * OpenTask
+ *
+ * Copyright (C) 2007 Jochen A. Rassler
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package opentask.data;
+
+import java.util.Calendar;
+
+/**
+ * @author rassler
+ *
+ */
+public class ActionItem {
+
+	private Calendar schedule;
+	private Calendar notifyTime;
+	private int duration;
+	private int nextNotification;
+	
+	public static int MINUTE = 1000 * 60; // Milliseconds
+	public static int HOUR = MINUTE * 60;
+	public static int DAY = HOUR * 24;
+	public static int WEEK = DAY * 7;
+	public static int DEFAULT_DELAY = 5 * MINUTE;
+	
+	public ActionItem() {
+		super();
+		schedule = Calendar.getInstance();
+		notifyTime = Calendar.getInstance();
+	}
+	
+	public ActionItem(Calendar schedule, Calendar notifyTime, int duration, int nextNotification) {
+		super();
+		this.schedule = schedule;
+		this.notifyTime = notifyTime;
+		this.duration = duration;
+		this.nextNotification = nextNotification;
+	}
+	
+	public ActionItem(Calendar schedule) {
+		super();
+		this.schedule = schedule;
+		notifyTime = schedule;
+		duration = 0;
+		nextNotification = DEFAULT_DELAY;
+	}
+	
+	public int getDuration() {
+		return duration;
+	}
+	
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	public int getNextNotification() {
+		return nextNotification;
+	}
+	
+	public void setNextNotification(int nextNotification) {
+		this.nextNotification = nextNotification;
+	}
+	
+	public Calendar getNotifyTime() {
+		return notifyTime;
+	}
+	
+	public void setNotifyTime(Calendar notifyTime) {
+		this.notifyTime = notifyTime;
+	}
+	
+	public Calendar getSchedule() {
+		return schedule;
+	}
+	
+	public void setSchedule(Calendar schedule) {
+		this.schedule = schedule;
+	}
+	
+}
