fb538f8292a08050065345bb50a9d14d245f955b
j_a_r <j_a_r@720f0306-7130-0410-9649-dcfab5623979>
2008-02-08 13:49:46 +0000

    refactoring

--- trunk/src/opentask/dialogs/NotifyDialog.java
+++ trunk/src/opentask/dialogs/NotifyDialog.java
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-package opentask;
+package opentask.dialogs;
 
 import java.awt.Frame;
 import javax.swing.*;
